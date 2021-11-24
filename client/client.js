2038a1c15b846c7652debaa6033d387e3fefd742
Eric Dropps <eric.dropps@gmail.com>
2021-11-24 08:26:59 -0700

    JS UX Tweak

--- client/client.js
+++ client/client.js
@@ -8,9 +8,9 @@ function getStripStatus(id) {
 
 function processResponse(data) {
     document.getElementById("strip-name-1").innerHTML=data.name
-    document.getElementById("strip-location-1").innerHTML=data.location
+    document.getElementById("strip-location-1").innerHTML=`${data.location} <span>  </span><span class="badge bg-info text-dark" id="strip-count-1">${data.led_count} LEDs</span>`
     document.getElementById("strip-description-1").innerHTML=data.description
-    document.getElementById("strip-count-1").innerHTML=`${data.led_count} LEDs` 
+    //document.getElementById("strip-count-1").innerHTML=`${data.led_count} LEDs` 
     document.getElementById("powerswitch").checked = data.power
     document.getElementById("rgb_value").value=data.single_color_hex
     document.getElementById("brightness").value = data.single_color_brightness
