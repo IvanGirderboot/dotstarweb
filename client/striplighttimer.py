82f1381b622060590d8193331223d4684ed1385a
kdropps87 <101026629+kdropps87@users.noreply.github.com>
2022-03-26 08:56:28 -0600

    Apply suggestions from code review
    Co-authored-by: Eric Dropps <eric.dropps@gmail.com>

--- client/striplighttimer.py
+++ client/striplighttimer.py
@@ -1,29 +1,33 @@
-import requests
-import time
 import datetime
-headers={'accept': 'application/json'}
-payload={'power':'true'}
-mario_API="http://mario.clients.shimley:8000/v1/strip/1/power"
-luigi_API="http://luigi.clients.shimley:8000/v1/strip/1/power"
+import time
+
+import requests
+HEADERS = {'accept': 'application/json'}
+MARIO_API="http://mario.clients.shimley:8000/v1/strip/1/power"
+LUIGI_API="http://luigi.clients.shimley:8000/v1/strip/1/power"
 def timer():
+       '''Turns LED strips on and off.'''
     while True: 
         
         now = datetime.datetime.today().timetuple()
         if now.tm_hour == 18 and now.tm_min == 0:
-            payload={'power':'true'}
-            m=requests.put(mario_API, headers=headers,params=payload)
-            l=requests.put(luigi_API, headers=headers,params=payload)
-        if now.tm_hour == 2 and now.tm_min == 0:
-            payload={'power':'false'}
-            m=requests.put(mario_API, headers=headers,params=payload)
-            l=requests.put(luigi_API, headers=headers,params=payload)
-        if m.status_code!= 200:
+            payload = {'power':'true'}
+            mario = requests.put(url=MARIO_API, headers=HEADERS, params=payload)
+            luigi = requests.put(url=LUIGI_API, headers=HEADERS, params=payload)
+        elif now.tm_hour == 2 and now.tm_min == 0:
+            payload = {'power':'false'}
+            mario = requests.put(url=MARIO_API, headers=HEADERS, params=payload)
+            luigi = requests.put(url=LUIGI_API, headers=HEADERS, params=payload)
+        else:
+            time.sleep(59)
+            continue
+        if mario.status_code!= 200:
             print("Mario looses a life!")
-        if l.status_code!= 200:
+        if luigi.status_code!= 200:
             print("Luigi Looses a Life")
         time.sleep(59)
         
-def main():
+if __name__ == "__main__":
     timer()
     
 
